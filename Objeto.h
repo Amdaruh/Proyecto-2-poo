c2adcef6319999921956c718821e67e79d7dd906
limartel <40374581+limartel@users.noreply.github.com>
2019-06-26 01:52:31 -0500

    segunda actualizacion

---
+++ Objeto.h
@@ -0,0 +1,33 @@
+//
+// Created by LI MARTEL on 26/06/2019.
+//
+#ifndef AGREGACION_OBJETO_H
+#define AGREGACION_OBJETO_H
+
+
+#include <iostream>
+#include "Tipos.h"
+
+using namespace std;
+
+class Objeto {
+private:
+    string        nombre;
+    TipoCaracter  color;
+    TipoEntero    posX;
+    TipoEntero    posY;
+public:
+    Objeto();
+    Objeto(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY);
+    virtual ~Objeto();
+    void setNombre(const TipoString& nombre);
+    string     getNombre();
+    TipoEntero getPosX();
+    TipoEntero getPosY();
+    char getColor();
+    void moverse(TipoEntero x, TipoEntero y);
+    string mostrarPosicion();
+};
+
+#endif //AGREGACION_OBJETO_H
---
+++ Tipos.h
@@ -0,0 +1,15 @@
+//
+// Created by LI MARTEL on 26/06/2019.
+//
+
+#ifndef AGREGACION_TIPOS_H
+#define AGREGACION_TIPOS_H
+
+#include <string>
+using namespace std;
+// Definiendo alias
+using TipoEntero = int;
+using TipoCaracter = char;
+using TipoString = string;
+
+#endif //AGREGACION_TIPOS_H
