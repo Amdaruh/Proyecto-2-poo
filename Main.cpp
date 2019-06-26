fb5bdc9fbe9e8b6ce32ff8833b8b4e5562e92d33
Amdaruh <40768026+Amdaruh@users.noreply.github.com>
2019-06-26 12:18:58 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
-
-//int main() {
-
-//    return 0;
-//}
+#include "Menu.h"
 
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
+int main() {
+ Menu menu;
+    menu.ejecutar();
     return 0;
 }
+
+
+//int main()
+//{
+  //  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    //sf::CircleShape shape(100.f);
+    //sh//ape.setFillColor(sf::Color::Green);
+
+   // while (window.isOpen())
+   // {
+    //    sf::Event event;
+  //      while (window.pollEvent(event))
+   //     {
+         //   if (event.type == sf::Event::Closed)
+       //         window.close();
+     //   }
+
+    //    window.clear();
+      //  window.draw(shape);
+        //window.display();
+   // }
+
+   // return 0;
+//}
